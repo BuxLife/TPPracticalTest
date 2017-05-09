e28274396f3f9b1b866a3d4b1a80a227a4994f50
BuxLife <Naeembux@live.com>
2017-05-09 15:44:00 +0200

    Response relationship implemented

--- src/main/java/Domains/Response.java
+++ src/main/java/Domains/Response.java
@@ -6,30 +6,37 @@ import java.util.Date;
  * Created by Bux_Life on 2017/05/09.
  */
 public class Response {
-    private String comment, ipAddress;
+    private String ipAddress, comment2;
     private Date date;
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+    private Comment comment;
 
     public String getIpAddress() {
         return ipAddress;
     }
-
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
-
     public Date getDate() {
         return date;
     }
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    public String getComment2() {
+        return comment2;
+    }
+
+    public void setComment2(String comment2) {
+        this.comment2 = comment2;
     }
 }
